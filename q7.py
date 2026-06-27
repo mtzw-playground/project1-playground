210d7e5dfb653ebf72c9106888b27caff462bac4
mtzw-playground <maythuzarwin2020@gmail.com>
2026-06-27 10:48:06 +0630

    Add files via upload

---
+++ q7.py
@@ -0,0 +1,13 @@
+char = input("Enter a single character: ")
+if char == 'a'or char == 'A':
+    print("It is a vowel")
+elif char == 'e' or char == 'E':
+    print("It is a vowel")
+elif char == 'i'or char == 'I':
+    print("It is a vowel") 
+elif char == 'o' or char == 'O':
+    print("It is a vowel")  
+elif char == 'u'or char == 'U':
+    print("It is a vowel")
+else:
+    print("It is a consonant")
